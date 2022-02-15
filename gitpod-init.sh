21ea1bc775ea10e2284a5334da19b1a633cb1b14
Sourabh Bajaj <sb2nov@gmail.com>
2022-02-15 08:45:09 -0800

    Remove the pgstart script

--- gitpod-init.sh
+++ gitpod-init.sh
@@ -1,7 +1,7 @@
 echo "Running the init script"
 
-# Auto-start PostgreSQL server.
-[[ $(pg_ctl status | grep PID) ]] || pg_start > /dev/null
+# # Auto-start PostgreSQL server.
+# [[ $(pg_ctl status | grep PID) ]] || pg_start > /dev/null
 
 psql -f /dbt/scripts/init.sql
 psql -d dbt -f /dbt/scripts/schema.sql
