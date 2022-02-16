602145cd2bf97e2b75cd3e3da3797799856d566f
Sourabh Bajaj <sb2nov@gmail.com>
2022-02-16 13:18:19 -0800

    Stabilize the gitpod start

--- gitpod-init.sh
+++ gitpod-init.sh
@@ -1,7 +1,4 @@
 echo "Running the init script"
 
-# Auto-start PostgreSQL server.
-[[ $(pg_ctl status | grep PID) ]] || pg_start > /dev/null
-
 psql -f /dbt/scripts/init.sql
 psql -d dbt -f /dbt/scripts/schema.sql
