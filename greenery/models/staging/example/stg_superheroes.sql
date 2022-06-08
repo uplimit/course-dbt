1e81cfee0b712134791af7b4964f8250fb968dfc
Jimmy Luong <jimmyvluong@gmail.com>
2022-06-08 21:57:53 +0000

    added in negative weight fix and text

--- greenery/models/staging/example/stg_superheroes.sql
+++ greenery/models/staging/example/stg_superheroes.sql
@@ -11,9 +11,9 @@ SELECT
     eye_color,
     race,
     hair_color,
-    height,
+    NULLIF(height, -99) AS height,
     publisher,
     skin_color,
     alignment,
-    weight
+    NULLIF(weight, -99) AS weight
 FROM {{ source('tutorial', 'superheroes') }}
