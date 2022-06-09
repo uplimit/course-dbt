49febcf09332f85ccaeb235b3ea68f5e53628ea9
mattizero <matilda@zeroheight.com>
2022-06-09 08:44:37 +0000

    reordering (easier reading)

--- greenery/models/greenery/staging/stg_addresses.sql
+++ greenery/models/greenery/staging/stg_addresses.sql
@@ -4,10 +4,12 @@
   )
 }}
 
+
+
 SELECT 
     address_id,
     address,
     zipcode,
-    state, -- Is this an optional field, and is this field available outside USA?
+    state, -- Is this an optional field, and/or is this field available outside USA?
     country
 FROM {{ source('src_greenery', 'addresses')}}
