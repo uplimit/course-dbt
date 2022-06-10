903ef5c0308db94404345bbe86ba9070cb2ccc78
Jimmy Luong <jimmyvluong@gmail.com>
2022-06-10 06:11:41 +0000

    adding CTEs and recasting columns

--- greenery/models/staging/staging_greenery/stg_addresses.sql
+++ greenery/models/staging/staging_greenery/stg_addresses.sql
@@ -1,13 +1,28 @@
 {{
   config(
-    materialized='table'
+    materialized= 'view'
   )
 }}
 
-SELECT
-  address_id,
-  address,
-  zipcode,
-  state,
-  country
-FROM {{ source('staging_greenery', 'addresses') }}
+with src_greenery_addresses as (
+  select * from {{ source('staging_greenery', 'addresses') }}
+)
+
+, renamed_recast as (
+  select
+    address_id,
+    address,
+    zipcode,
+    state,
+    country
+  from src_greenery_addresses
+)
+
+SELECT * FROM renamed_recast
+-- SELECT
+--   address_id,
+--   address,
+--   zipcode,
+--   state,
+--   country
+-- FROM {{ source('staging_greenery', 'addresses') }}
