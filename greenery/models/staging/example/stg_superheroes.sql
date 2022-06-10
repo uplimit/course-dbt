a47811cf5e36566badc1b57c02c804c5b6e41795
dextersworkshop <contactjasondexter@gmail.com>
2022-06-10 14:03:39 +0000

    updated model directory and organized examples

---
+++ greenery/models/staging/example/stg_superheroes.sql
@@ -0,0 +1,19 @@
+{{
+  config(
+    materialized='view'
+  )
+}}
+
+SELECT 
+    id AS superhero_id,
+    name,
+    gender,
+    eye_color,
+    race,
+    hair_color,
+    NULLIF(height, -99) AS height,
+    publisher,
+    skin_color,
+    alignment,
+    NULLIF(weight, -99) AS weight
+FROM {{ source('tutorial', 'superheroes') }}
