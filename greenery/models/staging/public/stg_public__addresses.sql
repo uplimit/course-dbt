a5b545a990cb9faa15ce51686f5eede82aaa6860
dextersworkshop <contactjasondexter@gmail.com>
2022-06-16 13:39:28 +0000

    debugged yaml errors and did a bit of project organization

--- greenery/models/staging/public/stg_public__addresses.sql
+++ greenery/models/staging/public/stg_public__addresses.sql
@@ -11,11 +11,11 @@ source as (
 standardized as (
 
 select
-    address_id as user_address_id,
-    address    as user_address,
-    zipcode    as user_zipcode,
-    state      as user_state,
-    country    as user_country
+      address_id as user_address_guid
+    , address    as user_address
+    , zipcode    as user_zip_code
+    , state      as user_state
+    , country    as user_country
 
 from source
 
