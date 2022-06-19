c562d4313df5ba25f63c3ca71c261b2dcb3eb04f
Jimmy Luong <jimmyvluong@gmail.com>
2022-06-19 21:35:48 +0000

    add int event model

---
+++ greenery/models/marts/product/intermediate/int_session_events_agg.sql
@@ -0,0 +1,15 @@
+{{
+  config(
+    materialized= 'table'
+  )
+}}
+
+SELECT
+  session_id
+  , user_id
+  , SUM(CASE WHEN event_type = 'add_to_cart' then 1 else 0 end) AS add_to_cart
+  , SUM(CASE WHEN event_type = 'checkout' then 1 else 0 end) AS checkout
+  , SUM(CASE WHEN event_type = 'page_view' then 1 else 0 end) AS page_view
+  , SUM(CASE WHEN event_type = 'package_shipped' then 1 else 0 end) AS package_shipped
+FROM {{ ref('stg_greenery__events') }}
+GROUP BY 1,2,3
