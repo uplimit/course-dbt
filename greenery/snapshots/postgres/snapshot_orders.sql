5a592d470205f7b72a807200bd44e4a8edcece25
jwmanahan <jwmanahan@gmail.com>
2022-10-06 14:37:27 +0000

    Add snapshot for orders -JM

---
+++ greenery/snapshots/postgres/snapshot_orders.sql
@@ -0,0 +1,16 @@
+{% snapshot snapshot_orders %}
+
+{{
+    config(
+        target_database = target.database
+        , target_schema = target.schema
+        , strategy='check'
+        , unique_key='order_id'
+        , check_cols=['status']
+    )
+}}
+
+SELECT *
+FROM {{ source('postgres', 'orders') }}
+
+{% endsnapshot %}
