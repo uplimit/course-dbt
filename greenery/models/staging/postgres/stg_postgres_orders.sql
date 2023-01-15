bd4f4c351c27116ea99916eaff295734d602c9d2
David-Adejumo <Daveadejumo@gmail.com>
2023-01-15 18:08:07 +0000

    Analytics engineering with dbt week a project

---
+++ greenery/models/staging/postgres/stg_postgres_orders.sql
@@ -0,0 +1,22 @@
+with src_orders as (
+    select * from {{ source('postgres', 'orders') }}
+)
+, renamed_recast as (
+  select 
+    ORDER_ID as order_guid
+    ,USER_ID as user_guid
+    ,PROMO_ID as promo_desc
+    ,ADDRESS_ID as address_guid
+    ,CREATED_AT::timestampntz as created_at_utc
+    ,ORDER_COST 
+    ,SHIPPING_COST
+    ,ORDER_TOTAL
+    ,TRACKING_ID as tracking_guid
+    ,SHIPPING_SERVICE
+    ,ESTIMATED_DELIVERY_AT::timestampntz as estimated_delivery_at_utc
+    ,DELIVERED_AT::timestampntz as delivery_at_utc
+    ,STATUS
+ from src_orders
+)
+
+select * from renamed_recast
