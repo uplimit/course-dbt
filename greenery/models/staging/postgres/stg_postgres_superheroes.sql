93583d9800e2d15f61940c8342ddd567f212b7ba
Dana Grunberg <danagrunberg@gmail.com>
2023-01-16 00:24:11 +0000

    add some nullifs

--- greenery/models/staging/postgres/stg_postgres_superheroes.sql
+++ greenery/models/staging/postgres/stg_postgres_superheroes.sql
@@ -10,16 +10,16 @@ WITH source_superheroes AS (
 
 , renamed_recast AS (
     SELECT id AS superhero_id
-           , name
+           , lower(REPLACE(name,' ','_')) AS name
            , lower(REPLACE(gender,' ','_')) AS gender
            , lower(REPLACE(eye_color,' ','_')) AS eye_color
            , lower(REPLACE(race,' ','_')) AS race
            , lower(REPLACE(hair_color,' ','_')) AS hair_color
-           , height
+           , NULLIF(height, -99) AS height
            , lower(REPLACE(publisher,' ','_')) AS publisher
            , lower(REPLACE(skin_color,' ','_')) AS skin_color
            , lower(REPLACE(alignment,' ','_')) AS alignment
-           , weight
+           , NULLIF(weight, -99) AS weight
            , created_at::timestamp AS created_at_utc
            , updated_at::timestamp AS updated_at_utc
     FROM source_superheroes
