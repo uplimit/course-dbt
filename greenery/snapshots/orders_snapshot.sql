56f8f327ebd0d6c6418aeb3f640d02057b2283a7
Gareth Brickman <gbrickman@pm.me>
2023-01-16 05:14:28 +0000

    add snapshot file

---
+++ greenery/snapshots/orders_snapshot.sql
@@ -0,0 +1,16 @@
+
+{% snapshot orders_snapshot %}
+
+    {{
+    config(
+        target_database = target.database,
+        target_schema = target.schema,
+        strategy='check',
+        unique_key='order_id',
+        check_cols=['status'],
+    )
+    }}
+
+    SELECT * FROM {{ source('postgres', 'orders') }}
+
+{% endsnapshot %}
