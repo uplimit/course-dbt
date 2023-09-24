14f124cdcf25cf1119b24d909d6e17deb0edefce
Y P <fierien@yandex.ru>
2023-09-24 18:52:18 +0000

    week 2

---
+++ greenery/models/intermediate/int_session_timing.sql
@@ -0,0 +1,12 @@
+{{
+  config(
+    materialized='view'
+  )
+}}
+
+select
+    session_id,
+    min(created_at) as session_started_at,
+    max(created_at) as session_ended_at
+from {{ ref('stg_postgres__events') }}
+group by 1
