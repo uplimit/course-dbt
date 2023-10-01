96864acbfdd80f2a24cff09fe605b16ce9302f87
Y P <fierien@yandex.ru>
2023-10-01 09:02:25 +0000

    week 3

--- greenery/models/marts/product/fact_page_views.sql
+++ greenery/models/marts/product/fact_page_views.sql
@@ -4,6 +4,11 @@
   )
 }}
 
+{% set event_types = dbt_utils.get_column_values(
+  table=ref('stg_postgres__events'), 
+  column='event_type') 
+%}
+
 
 with session_timing as (
   select * from {{ ref('int_session_timing') }}
@@ -15,10 +20,9 @@ select
     coalesce(e.product_id, oi.product_id) as product_id,
     session_started_at,
     session_ended_at,
-    sum(case when event_type = 'checkout' then 1 else 0 end) as count_checkout,
-    sum(case when event_type = 'package_shipped' then 1 else 0 end) as count_package_shipped,
-    sum(case when event_type = 'add_to_cart' then 1 else 0 end) as count_add_to_cart,
-    sum(case when event_type = 'page_view' then 1 else 0 end) as count_page_view,
+    {% for event_type in event_types %}
+    {{ sum_of('e.event_type', event_type )}} as {{ event_type }}s,
+    {% endfor %}
     datediff('minute', session_started_at, session_ended_at) as session_length
     from {{ ref('stg_postgres__events') }} e
 left join {{ ref('stg_postgres__order_items') }} oi using (order_id)
