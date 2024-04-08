f1ea43467efb59e3213913620b7720419b8df501
Vidya Gopalan <gops.vidya@gmail.com>
2024-04-08 03:22:59 +0000

    used get_column_values macro from  dbt utils package

--- greenery/models/marts/product/fact_page_views.sql
+++ greenery/models/marts/product/fact_page_views.sql
@@ -3,12 +3,7 @@ events as ( select * from {{ref('stg_postgres__events')}}),
 order_items as ( select * from {{ref('stg_postgres__order_items')}}),
 session_agg as ( select * from {{ref('int_session_dates')}})
 
-{% set event_types = [
- 'page_view',
- 'add_to_cart',
- 'checkout',
- 'package_shipped'
-] %}
+{% set event_types = dbt_utils.get_column_values(table=ref('stg_postgres__events'), column='event_type')  %}
 
 select 
     e.session_id,
